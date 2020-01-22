9ccd5b10de264cc7de5ed8434ac0ee011eed7660
Andrew Welch <andrew@nystudio107.com>
2020-01-22 09:51:57 -0500

    If we're in `devMode` invalidate the cache immediately
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

--- src/helpers/Manifest.php
+++ src/helpers/Manifest.php
@@ -505,6 +505,10 @@ EOT;
         $cacheDuration = Craft::$app->getConfig()->getGeneral()->devMode
             ? self::DEVMODE_CACHE_DURATION
             : null;
+        // If we're in `devMode` invalidate the cache immediately
+        if (Craft::$app->getConfig()->getGeneral()->devMode) {
+            self::invalidateCaches();
+        }
         // Get the result from the cache, or parse the file
         $cache = Craft::$app->getCache();
         $settings = Twigpack::$plugin->getSettings();
