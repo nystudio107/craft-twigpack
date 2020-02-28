cde66ec1615d0bad3f3752bc23adc50510b9ab20
Andrew Welch <andrew@keluli.local>
2020-02-28 13:50:39 -0500

    Updated docs path

--- docs/docs/.vuepress/config.js
+++ docs/docs/.vuepress/config.js
@@ -2,7 +2,7 @@ module.exports = {
     title: 'Twigpack Documentation',
     description: 'Documentation for the Twigpack plugin',
     base: '/docs/twigpack/',
-    dest: '../../../../sites/nystudio107/web/docs/twigpack',
+    dest: '../../../sites/nystudio107/web/docs/twigpack',
     themeConfig: {
         displayAllHeaders: true,
         sidebar: [
