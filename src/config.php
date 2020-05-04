d738a98aefcb1eac8472f9317f3d8d16eb107cfc
Andrew Welch <andrew@keluli.local>
2020-05-04 14:49:33 -0400

    Note that `errorEntry` can be a string or an array of strings

--- src/config.php
+++ src/config.php
@@ -31,6 +31,7 @@ return [
         // Enforce Absolute URLs on includes
         'useAbsoluteUrl' => true,
         // The JavaScript entry from the manifest.json to inject on Twig error pages
+        // This can be a string or an array of strings
         'errorEntry' => '',
         // String to be appended to the cache key
         'cacheKeySuffix' => '',
