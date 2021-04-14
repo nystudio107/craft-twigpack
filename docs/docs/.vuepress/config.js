38e146b8111285b04898668aac0aacc9a65c7421
Andrew Welch <andrew@nystudio107.com>
2021-04-13 22:07:11 -0400

    Added buildchain for docs
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

--- docs/docs/.vuepress/config.js
+++ docs/docs/.vuepress/config.js
@@ -2,7 +2,6 @@ module.exports = {
     title: 'Twigpack Documentation',
     description: 'Documentation for the Twigpack plugin',
     base: '/docs/twigpack/',
-    dest: '../../../sites/nystudio107/web/docs/twigpack',
     themeConfig: {
         displayAllHeaders: true,
         sidebar: [
