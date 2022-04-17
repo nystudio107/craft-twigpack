b993dc91df938809da2d933ea769a28e9b258b63
Andrew Welch <andrew@nystudio107.com>
2022-04-17 12:38:49 -0400

    fix: Fix incorrect `User-Agent` header that could cause an error to be thrown

--- src/helpers/Manifest.php
+++ src/helpers/Manifest.php
@@ -616,7 +616,7 @@ EOT;
                     try {
                         $response = $client->request('GET', $path, [
                             RequestOptions::HEADERS => [
-                                'User-Agent' => "User-Agent:Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13",
+                                'User-Agent' => "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13",
                                 'Accept' => '*/*',
                             ],
                         ]);
